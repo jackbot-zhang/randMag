943a691cb7e42d8638799732e5a709180af352d7
wuqingping <978746873@qq.com>
2024-11-19 14:35:51 +0800

    .

--- go.mod
+++ go.mod
@@ -3,13 +3,17 @@ module randMag
 go 1.23.2
 
 require (
-	gorm.io/driver/sqlite v1.5.6
-	gorm.io/gorm v1.25.12
+	github.com/mattn/go-sqlite3 v1.14.22
+	xorm.io/xorm v1.3.9
 )
 
 require (
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/goccy/go-json v0.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
 )
--- main.go
+++ main.go
@@ -2,73 +2,181 @@ package main
 
 import (
 	"bufio"
-	"database/sql"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
+	"xorm.io/xorm"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
+var engine *xorm.Engine
+
+// TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+var magList []string
 
 func main() {
-	// 打开数据库连接
-	db, err := sql.Open("sqlite3", "D:\\mycode\\randMag\\test.db")
+	magList = []string{}
+	var err error
+	engine, err = xorm.NewEngine("sqlite3", "./test.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-	// 查询数据
-	rows, err := db.Query("SELECT id, url  FROM projects where  view_cnt is null  limit 10")
+	defer engine.Close()
+	err = engine.Sync(mag{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
-	var idList []int64
-	for rows.Next() {
-		var id int64
-		var url string
-		err = rows.Scan(&id, &url)
-		if err != nil {
-			log.Fatal(err)
+	reader := bufio.NewReader(os.Stdin)
+	txt := "请选择命令：\n 1.读取不删除\n 2.读取删除\n 3.标准输入\n 4.文件输入\n 5.退出\n"
+	fmt.Print(txt)
+	for {
+		input, _ := reader.ReadString('\n') // 读取用户输入
+		input = strings.TrimSpace(input)
+		switch input {
+		case "1":
+			printTen()
+		case "2":
+			printAndUpdate()
+		case "3":
+			fmt.Println("请输入磁力链接:")
+			inputMode(reader)
+			fmt.Print(txt)
+		case "4":
+			fmt.Println("请输入文件名:")
+			inputFile(reader)
+			time.Sleep(1 * time.Second)
+			fmt.Printf("\n文件插入完成,%s", txt)
+		case "5":
+			return
+		case "":
+			fmt.Print(txt)
+		default:
+			fmt.Println("拜托，别乱选")
 		}
-		fmt.Println(url)
-		idList = append(idList, id)
-	}
-	// 检查查询过程中可能出现的错误
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("输入OK,更新并且退出")
-	// 捕获 Ctrl+C 信号
-	scanner := bufio.NewScanner(os.Stdin)
 
-	for scanner.Scan() {
-		input := strings.TrimSpace(scanner.Text())
-		if strings.ToLower(input) == "ok" {
-			// 更新数据
-			for _, id := range idList {
-				_, err = db.Exec("UPDATE projects SET view_cnt = ? WHERE id = ?", 1, id)
-				if err != nil {
-					log.Printf("Failed to update view_cnt for project ID %d: %v", id, err)
+	}
+
+}
+func inputFile(reader *bufio.Reader) {
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	file, err := os.Open(input)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	bt, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	arr := strings.Split(string(bt), "\n")
+	magnetRegex := `magnet:\?xt=urn:btih:[0-9a-zA-Z]{32,40}`
+	re := regexp.MustCompile(magnetRegex)
+	cnt := 0
+	for _, s := range arr {
+		// 查找哈希值
+		match := re.FindAllString(s, -1)
+		if match != nil {
+			for _, magnet := range match {
+				_, err = engine.InsertOne(mag{Url: magnet})
+				if err == nil {
+					cnt++
 				}
 			}
-			log.Println("更新完成，程序退出")
-			time.Sleep(time.Second * 2)
-			return
 		}
 	}
+	fmt.Printf("\n一共成功插入%d条数据\n", cnt)
+}
+func printTen() {
+	list, err := selectTen()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	for _, m := range list {
+		fmt.Println(m.Url)
+	}
+}
+func printAndUpdate() {
+	list, err := selectTen()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	for _, m := range list {
+		fmt.Println(m.Url)
+		engine.ID(m.ID).MustCols("view_cnt").Update(mag{ViewCnt: 1})
+	}
+}
+func selectTen() ([]*mag, error) {
+	var x []*mag
+	err := engine.Table(mag{}).Where("view_cnt is null or  view_cnt is null = 0").Limit(10).Find(&x)
+	return x, err
+}
+func inputMode(reader *bufio.Reader) {
+	magnetRegex := `magnet:\?xt=urn:btih:[0-9a-zA-Z]{32,40}`
+	re := regexp.MustCompile(magnetRegex)
+	continuousInputMode := false
+	cnt := 0
+	var t1 *time.Time
+	for {
+		input, _ := reader.ReadString('\n') // 读取用户输入
+		t2 := time.Now()
+		if t1 != nil {
+			if t2.Sub(*t1) < 100*time.Millisecond {
+				continuousInputMode = true
+				tmp := time.Now()
+				t1 = &tmp
+			} else {
+				continuousInputMode = false
+				t1 = nil
+				fmt.Println("输入结束,共插入", cnt, "条数据。请按回车键")
+				cnt = 0
+			}
+		}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("Reading standard input: %v", err)
+		//
+		input = strings.TrimSpace(input)
+		if !continuousInputMode && input == "back" {
+			return
+		} else if !continuousInputMode && input == "" {
+			fmt.Println("请输入磁力链接,或输入back返回:")
+		} else {
+			//第一次进来更新首次时间
+			if t1 == nil {
+				tmp := time.Now()
+				t1 = &tmp
+			}
+			dealLine(input, re, &cnt)
+		}
+	}
+}
+
+func dealLine(line string, re *regexp.Regexp, cnt *int) {
+	match := re.FindAllString(line, -1)
+	if match != nil {
+		for _, magnet := range match {
+			_, err := engine.InsertOne(mag{Url: magnet})
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			if err == nil {
+				*cnt++
+			}
+		}
 	}
 
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+type mag struct {
+	ID      int    `xorm:"id pk autoincr"`
+	Url     string `xorm:"url unique"`
+	ViewCnt int    `xorm:"view_cnt"`
+}
